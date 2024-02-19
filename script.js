5e478c7f556156bf5d0ae5b9b5edc88a460bcad4
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-02-19 22:11:24 +0600

    count problem solve

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let seats = document.getElementsByClassName('seats');
+let count = 0;
+let leftSeat = 8;
 let totalCost = 0;
 let getSeat = 0;
 // let seatCount = 0;
 for (let seat of seats){
     seat.addEventListener('click', function(){
+        // seat.classList.add('howkerlkdfds');
+        console.log(seat)
+        
         
         seat.style.backgroundColor = '#1DD100';
         // decrease seat number
@@ -11,25 +16,29 @@ for (let seat of seats){
         let totalSeatText = totalSeatElement.innerText;
         let totalSeat = parseInt(totalSeatText);
         let availableSeat = totalSeat - 1;
-        totalSeatElement.innerText = availableSeat
+        
 
         // select sect number
         let selectSeatElement = document.getElementById('select-seat')
         let selectSeatElementText = selectSeatElement.innerText;
         let selectSeat = parseInt(selectSeatElementText);
-        let getSeat = selectSeat + 1
-        selectSeatElement.innerText = getSeat
-
-        if(getSeat > 4){
-            //seat.target.setAttribute("disabled",true)
+        
+        
+        if(count > 3){
+            // seat.classList.add("pointer-events-none")
+            seat.setAttribute('disabled',true);
+            // seat.classList.remove('seats')
             seat.style.backgroundColor = '';
             alert('apni 4tar beshi seat select korte parben nah')
+            
             return;
             
         }
-        getSeat++
+        leftSeat = leftSeat - 1;
         
-        
+        count = count  + 1
+        selectSeatElement.innerText = count
+        totalSeatElement.innerText = leftSeat
        
         // price 
         let totalAmount = document.getElementById('total-amount');
